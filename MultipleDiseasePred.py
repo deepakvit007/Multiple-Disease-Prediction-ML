73b4488f6027f6fc285d2ee5db23bc221d4227c0
deepakvit007 <deepak007vit@gmail.com>
2022-06-29 20:58:32 +0530

    final error fixed

--- MultipleDiseasePred.py
+++ MultipleDiseasePred.py
@@ -6,11 +6,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('Things Required For Deployment\diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('Things Required For Deployment\heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('Things Required For Deployment\parkinson_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('parkinson_model.sav', 'rb'))
 
 
 
